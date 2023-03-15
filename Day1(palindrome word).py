335bf511ecba29c327d81e7f4a68ba8954587400
Mark Daniel Lampa <jonaxxmontefalco7@gmail.com>
2023-03-15 22:18:00 +0800

    Update Day1(palindrome word).py

---
+++ Day1(palindrome word).py
@@ -0,0 +1,20 @@
+def palindrome(sentence):
+    #replace all punctuation character into "" 
+    for i in (",.'?/}{[]'"):
+        sentence=sentence.replace(i,"")
+    #declare a list to store the data temporarily
+    palindrome=[]
+    #
+    word=sentence.split(' ')
+    for i in word:
+        i=i.lower()
+        if i==i[::-1]:
+            palindrome.append(i)
+    return palindrome
+
+sentence=input("Enter a sentence: ")
+print(palindrome(sentence))
+
+
+
+
